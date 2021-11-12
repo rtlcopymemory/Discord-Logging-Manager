883f371f18992a1f5952f67f11f1867481b59a23
ThatOnePizza <info@protocli.me>
2021-11-12 18:34:36 +0100

    does the database add thingy

---
+++ src/db.js
@@ -0,0 +1,19 @@
+const sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database(process.env.DB_PATH);
+
+db.serialize(function () {
+    db.run("CREATE TABLE IF NOT EXISTS servers (serverID TEXT PRIMARY KEY, channelID TEXT DEFAULT '')");
+});
+
+const ERRORS = {
+    GENERIC: 0,
+    DUPLICATE: 1
+}
+
+Object.freeze(ERRORS);
+
+module.exports = {
+    db,
+    ERRORS
+}
--- src/interactions/logging.js
+++ src/interactions/logging.js
@@ -1,13 +1,48 @@
-const { Interaction } = require("discord.js");
+const { Interaction, Channel } = require("discord.js");
+const { db, ERRORS } = require("../db");
 
 /**
  * 
  * @param {Interaction} interaction 
  */
 async function handleLogging(interaction) {
+    // Gotta check if moderator
+    const serverID = interaction.guildId;
+
     switch (interaction.options.getSubcommand()) {
         case "set":
-            await interaction.reply("You used set");
+            /** @type {Channel} channel */
+            let channel = interaction.options.getChannel('target');
+            if (!channel) {
+                await interaction.reply("Channel not valid");
+                return;
+            }
+
+            let error = -1;
+            db.get("SELECT * FROM servers WHERE serverID = ?", [serverID], (err, row) => {
+                if (!!err) {
+                    error = ERRORS.GENERIC;
+                    return;
+                }
+
+                if (!!row) {
+                    error = ERRORS.DUPLICATE;
+                }
+            })
+
+            if (error === ERRORS.GENERIC) {
+                await interaction.reply("An error has accured");
+                return;
+            } else if (error === ERRORS.DUPLICATE) {
+                await interaction.reply("Server already existing. Use remove");
+                return;
+            }
+
+            var stmt = db.prepare("INSERT INTO servers (serverID, channelID) VALUES (?, ?)");
+            stmt.run(serverID, channel.id);
+            stmt.finalize();
+
+            await interaction.reply(`Channel <#${channel.id}> set`);
             break;
         case "remove":
             await interaction.reply("You used remove");
