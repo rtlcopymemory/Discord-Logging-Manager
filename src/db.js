a5cb22a9bb1cc6627a652769455f32807665136c
ThatOnePizza <info@protocli.me>
2021-11-12 18:57:39 +0100

    deletes unused export

--- src/db.js
+++ src/db.js
@@ -6,15 +6,6 @@ db.serialize(function () {
     db.run("CREATE TABLE IF NOT EXISTS servers (serverID TEXT PRIMARY KEY, channelID TEXT DEFAULT '')");
 });
 
-const ERRORS = {
-    GENERIC: 0,
-    DUPLICATE: 1,
-    NOT_FOUND: 2
-}
-
-Object.freeze(ERRORS);
-
 module.exports = {
-    db,
-    ERRORS
+    db
 }
